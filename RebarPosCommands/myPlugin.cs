255483a54f8e41a0ecf3ed5078e0d35c76763d44
oozcitak <ozcitak@yahoo.com>
2012-04-26 15:39:06 +0300

    Added menu icons

---
+++ RebarPosCommands/myPlugin.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System.Reflection;
+using System.IO;
+using System.Collections.Generic;
+using System;
+
+// This line is not mandatory, but improves loading performances
+[assembly: ExtensionApplication(typeof(RebarPosCommands.RebarPosPlugin))]
+
+namespace RebarPosCommands
+{
+    // This class is instantiated by AutoCAD once and kept alive for the 
+    // duration of the session. If you don't do any one time initialization 
+    // then you should remove this class.
+    public class RebarPosPlugin : IExtensionApplication
+    {
+        void IExtensionApplication.Initialize()
+        {
+            
+        }
+
+        void IExtensionApplication.Terminate()
+        {
+            
+        }
+    }
+}
